5784e80b99f7660fb6121ffa4385967226995a2b
Ume-Aham Sylvester <117973007+gittybro12@users.noreply.github.com>
2024-07-24 00:14:32 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,47 @@
+def Add(num1,num2):
+    return num1 + num2
+
+def Subtract(num1,num2):
+    return num1 - num2
+
+def multiply(num1,num2):
+    return num1 * num2
+
+def divide(num1,num2):
+    return num1 / num2
+
+functions = {
+    '+' : Add,
+    '-' : Subtract,
+    '*' : multiply,
+    '/' : divide
+
+} 
+def calculator():
+    num1 = float(input("what is the first number? "))
+    for x in functions.keys():
+        print(f'{x}')
+    operation = input("Enter the operation you want to perform ")
+    num2 = float(input("what is the second number? "))
+
+    operation_symbol = functions[operation]
+    run = operation_symbol(num1=num1,num2=num2)
+
+
+    print(f"{num1} {operation} {num2} = {run}")
+
+    still_in = False
+    while not still_in:
+        cont = input(f"Type 'y' to continue calculating with {run} or 'no' to start a new calculation: ")
+
+        if cont == 'no':
+            still_in = True
+            calculator()
+        else:    
+            operation = input("pick another operation? ")
+            num3 = float(input("Enter a number? "))
+            operation_symbol = functions[operation]
+            run2 = operation_symbol(run,num3)
+
+            print(f"{run} {operation} {num3} = {run2}")
+calculator()
